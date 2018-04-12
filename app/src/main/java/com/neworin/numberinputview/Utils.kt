dc025fe4b42150dc7f14d70c3abff5e0a5790233
zhangfubin <zhangfubin@91zdan.com>
2018-04-12 18:16:50 +0800

    添加工具类

---
+++ app/src/main/java/com/neworin/numberinputview/Utils.kt
@@ -0,0 +1,41 @@
+package com.neworin.numberinputview
+
+import android.content.Context
+import android.graphics.Point
+import android.os.Build
+import android.view.WindowManager
+
+/**
+ * author : ZhangFubin
+ * time   : 2018/04/12
+ * desc   :
+ */
+fun dp2px(context: Context, dpValue: Float): Int {
+    val scale = context.resources.displayMetrics.density
+    return (dpValue * scale + 0.5f).toInt()
+}
+
+/**
+ * 获取屏幕的宽度（单位：px）
+ *
+ * @return 屏幕宽
+ */
+fun getScreenWidth(context: Context): Int {
+    val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    val point = Point()
+    wm.defaultDisplay.getSize(point)
+    return point.x
+}
+
+/**
+ * 获取屏幕的高度（单位：px）
+ *
+ * @return 屏幕高
+ */
+fun getScreenHeight(context: Context): Int {
+    val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    val point = Point()
+    wm.defaultDisplay.getSize(point)
+    return point.y
+}
+
